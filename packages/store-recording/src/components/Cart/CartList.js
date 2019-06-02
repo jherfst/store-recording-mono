7059a241bb23c835d6ba9543f5b6294e45b3bb4c
HerfstJ <Josh.Herfst@rabobank.nl>
2019-06-02 14:21:24 +0200

    initial commit

---
+++ packages/store-recording/src/components/Cart/CartList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CartItem from './CartItem';
+
+export default class CartList extends Component {
+
+    render() {
+        const {cart} = this.props.value;
+        return (
+            <div className="container-fluid">
+                {cart.map(item => <CartItem key={item.id} item={item} value={this.props.value}/>)}
+            </div>
+        )
+    }
+}
