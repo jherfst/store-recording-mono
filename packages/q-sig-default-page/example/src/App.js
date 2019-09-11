8d145e164eef8955191d0e8ac2745fdea966e245
HerfstJ <Josh.Herfst@rabobank.nl>
2019-09-11 12:27:34 +0200

    pre changes

--- packages/q-sig-default-page/example/src/App.js
+++ packages/q-sig-default-page/example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'q-sig-default-page'
+import Default from 'q-sig-default-page'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Default text='Modern React component module' />
       </div>
     )
   }
--- packages/q-sig-default-page/src/index.js
+++ packages/q-sig-default-page/src/index.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export default class Default extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-white text-uppercase pt-5">
+                        <h1 className="display-3">404</h1>
+                        <h1>error</h1>
+                        <h2>page not found</h2>
+                        <h3>the requested URL{" "}
+                            <span className="text-danger">
+                            {this.props.location && this.props.location.pathname}
+                            </span>{" "}
+                             was not found
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
