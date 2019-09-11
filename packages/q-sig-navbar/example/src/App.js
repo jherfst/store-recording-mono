ffcf4b432cc768ed6e4cb0b03774ef8eb4bbeeaa
HerfstJ <Josh.Herfst@rabobank.nl>
2019-09-11 12:27:57 +0200

    pre changes

--- packages/q-sig-navbar/example/src/App.js
+++ packages/q-sig-navbar/example/src/App.js
@@ -5,9 +5,7 @@ import ExampleComponent from 'q-sig-navbar'
 export default class App extends Component {
   render () {
     return (
-      <div>
         <ExampleComponent text='Modern React component module' />
-      </div>
     )
   }
 }
--- packages/q-sig-navbar/src/index.js
+++ packages/q-sig-navbar/src/index.js
@@ -1,22 +1,52 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from './main-logo.svg';
+import ButtonContainer from 'q-sig-button';
+import styled from 'styled-components';
+import { BrowserRouter as Router } from "react-router-dom";
 
-import styles from './styles.css'
+export default class Navbar extends Component {
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
 
-  render() {
-    const {
-      text
-    } = this.props
+    render() {
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+        return (
+           <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+              {/*
+                https://www.iconfinder.com/icons/1243689/call_phone_icon
+                Creative Commons (Attribution 3.0 Unported);
+                https://www.iconfinder.com/Makoto_msk
+            */}
+            <Router>
+                <Link to='/' replace>
+                    <img src={logo} alt="store" className="navbar-brand"  />
+                </Link>
+                <ul className="navbar-nav align-item-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link" >
+                            Car Stock
+                        </Link>
+                    </li>
+                </ul>
+                {/* <Link to="/cart" className="ml-auto" replace>
+                    <ButtonContainer>
+                        <span className="mr-2 text-white">
+                            <i className="fas fa-cart-plus"/>
+                            My cart
+                        </span>
+                    </ButtonContainer>
+                </Link> */}
+            </Router>
+           </NavWrapper>
+        )
+    }
 }
+
+const NavWrapper = styled.nav`
+   background: var(--mainBlue)!important;
+   .nav-link {
+       color: var(--mainWhite)!important;
+       font-size: 1.3rem;
+       text-transform: capitalize;
+   }
+`;
